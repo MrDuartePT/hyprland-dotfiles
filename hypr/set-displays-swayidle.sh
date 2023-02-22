49a7e4c1f31242380bbf43d1a2f2a7953830168b
Gonçalo Duarte <gonegrier.duarte@gmail.com>
2023-02-22 18:50:20 +0000

    migrate to gtklock

--- hypr/set-displays-swayidle.sh
+++ hypr/set-displays-swayidle.sh
@@ -19,21 +19,35 @@ if [ $POWER_ADAPTER ]; then; refresh_rate="165hz" && time_1=300 && time_2=305 &&
 #Set Internal Display Variable
 if [ $DISP_LAPTOP_NVIDIA ]; then
   #Nvidia Optimus Enable
-  export laptop=eDP-1
+  INTERNAL_DISPLAY=eDP-1
   hyprpaper -c $HOME/.config/hypr/hyprpaper.conf &
 else
   #Nvidia Optimus Disable
-  export laptop=eDP-2
+  INTERNAL_DISPLAY=eDP-2
   hyprpaper -c $HOME/.config/hypr/hyprpaper-nvidia.conf &
 fi
 
 #Set Displays Resolutions
-if [ $EXT_MONITOR ]; then; hyprctl keyword monitor HDMI-A-1,1920x1080,0x0,1 && laptop_display_pos="0x1080"; else laptop_display_pos="0x0"; fi
-hyprctl keyword monitor $laptop,1920x1080@$refresh_rate,$laptop_display_pos,1
+
+#old position
+# if [ $EXT_MONITOR ]; then; hyprctl keyword monitor HDMI-A-1,1920x1080,0x0,1 && laptop_display_pos="0x1080"; else laptop_display_pos="0x0"; fi
+# hyprctl keyword monitor $laptop,1920x1080@$refresh_rate,$laptop_display_pos,1
+
+hyprctl keyword monitor $laptop,1920x1080@$refresh_rate,0x0,1
+hyprctl keyword monitor HDMI-A-1,1920x1080,1920x0,1
 
 #Set Swayidle For Suspend
+
+## GTKLock command
 swayidle -w \
-  timeout $time_1 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' \
-  timeout $time_2 'grim -s 0.1 -g "0,1080 1920x1080" $HOME/.config/hypr/screenlockbg.png && swaylock -f --clock --indicator  -i $HOME/.config/hypr/screenlockbg.png -s fill' \
+  timeout $time_1 'grim -s 0.1 -g "0,0 1920x1080" $HOME/.config/hypr/screenlockbg.png && hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' \
+  timeout $time_2 'gtklock -d -m /usr/lib/gtklock/playerctl-module.so -m /usr/lib/gtklock/powerba -b $HOME/.config/hypr/screenlockbg.png' \
   timeout $time_3 'systemctl suspend' \
-  before-sleep 'swaylock -f --clock  --grace 5 --indicator  -i $HOME/.config/hypr/screenlockbg.png -s fill' &
+  before-sleep 'gtklock -d -m /usr/lib/gtklock/playerctl-module.so -m /usr/lib/gtklock/powerbar-module.so -m /usr/lib/gtklock/userinfo-module.so -b $HOME/.config/hypr/screenlockbg.png' &
+
+## Swaylock command
+#swayidle -w \
+  #timeout $time_1 'grim -s 0.1 -g "0,0 1920x1080" $HOME/.config/hypr/screenlockbg.png && hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' \
+  #timeout $time_2 'swaylock -f --clock --indicator  -i $HOME/.config/hypr/screenlockbg.png -s fill' \
+  #timeout $time_3 'systemctl suspend' \
+  #before-sleep 'swaylock -f --clock  --grace 5 --indicator  -i $HOME/.config/hypr/screenlockbg.png -s fill' &
