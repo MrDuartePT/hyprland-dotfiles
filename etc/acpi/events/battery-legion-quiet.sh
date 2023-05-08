978f113753ceb4fc0fe47d2ff76bae1f1b2e7391
Gonçalo Duarte <gonegrier.duarte@gmail.com>
2023-05-08 13:06:45 +0100

    little correction

--- etc/acpi/events/battery-legion-quiet.sh
+++ etc/acpi/events/battery-legion-quiet.sh
@@ -2,12 +2,11 @@
 
 ADP0_connected=$(cat /sys/class/power_supply/ADP0/online)
 
-/bin/systemctl restart lenovo-fancurve.service
-
 if [[ $ADP0_connected -eq 1 ]]; then
 	/bin/echo balanced > /sys/firmware/acpi/platform_profile
 else
 	/bin/echo quiet > /sys/firmware/acpi/platform_profile
 fi
 
+/bin/systemctl restart lenovo-fancurve.service
 
